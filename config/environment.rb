f7daa06e007da636e789f0bd913f6120955b8593
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-18 06:34:25 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require_relative '../lib/dog'
 
 DB = {:conn => SQLite3::Database.new("db/dogs.db")}
 
-#
+# is set equal to a hash, which has a single key, :conn. The key, :conn, will have a value of a connection to a sqlite3 database in the db directory.

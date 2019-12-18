5e4eebffd1b721007cdcbfefbbff4012288e87fb
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-18 06:58:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,6 +43,8 @@ class Dog
     sql = <<-SQL
       SELECT * FROM dogs WHERE id = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql, id)
+    DB[:conn].execute(sql, id).map do |row|
+         self.new_from_db(row)
+      end
   end
 end
